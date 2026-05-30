b18d431eb1dc62cecad643079f8826d3a2205f74
Isadora Melo <isadora.cunha@estudante.ufla.br>
2026-05-30 17:27:45 -0300

    Adicionando funcionalidade de autenticação

--- core/views.py
+++ core/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/login/')
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+def cadastro_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'cadastro.html', {'form': form})
--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from core import views
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('cadastro/', views.cadastro_view, name='cadastro'),
+    path('', views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
